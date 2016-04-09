4e5d22fc5b6c18bea1fceb17d83c651b01164daf
Kyle Chapman <xeuoruxgmail.com>
2016-04-09 13:19:16 -0700

    Basic gamestates setup

--- CSE360TeamProject/src/cse360/team6/DiceGame.java
+++ CSE360TeamProject/src/cse360/team6/DiceGame.java
@@ -1,18 +1,37 @@
 package cse360.team6;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class DiceGame extends StateBasedGame
 {
-
-	public DiceGame(String name) {
+	public static final int MAIN = 0;
+	public static final int SETUP = 1;
+	public static final int RULES = 2;
+	public static final int STATISTICS = 3;
+	public static final int GAME = 4;
+	
+	public static void main (String[] args)
+	{
+		try
+		{
+			AppGameContainer gameContainer = new AppGameContainer(new DiceGame("37 Game"), 600, 600, false);
+		} catch (SlickException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public DiceGame(String name)
+	{
 		super(name);
 	}
 
 	@Override
-	public void initStatesList(GameContainer arg0) throws SlickException {
+	public void initStatesList(GameContainer arg0) throws SlickException
+	{
 		// TODO Auto-generated method stub
 		
 	}
