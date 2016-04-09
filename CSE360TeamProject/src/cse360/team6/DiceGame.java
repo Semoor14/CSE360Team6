0a5609e20bf8f764076b703dc89ed8395ff60c3e
Kyle Chapman <xeuoruxgmail.com>
2016-04-09 13:03:48 -0700

    DiceGame class

--- CSE360TeamProject/src/cse360/team6/DiceGame.java
+++ CSE360TeamProject/src/cse360/team6/DiceGame.java
@@ -1,6 +1,20 @@
 package cse360.team6;
 
-public class DiceGame
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class DiceGame extends StateBasedGame
 {
+
+	public DiceGame(String name) {
+		super(name);
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
