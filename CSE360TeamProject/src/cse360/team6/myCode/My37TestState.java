7d4ab7ccd27e07b75aaf65880885dac33c48ba11
Nicholas <nsstanto@asu.edu>
2016-04-15 14:08:11 -0700

    Can Roll Dice.

--- CSE360TeamProject/src/cse360/team6/myCode/My37TestState.java
+++ CSE360TeamProject/src/cse360/team6/myCode/My37TestState.java
@@ -15,14 +15,53 @@ public class My37TestState implements GameState
 {
 	private MyLargeQueue largeQueue;
 	private MyPlayer player1;
+	private MyPlayer player2; 
+	private MyPlayer player3;
+	private MyPlayer player4;
 	private MyDiceHandler diceHandler;
 	
-	private int endTurn_XPos;
-	private int endTurn_YPos;
-	private int endTurn_Width;
-	private int endTurn_Height;
+	private MyCenteredTextButton endTurnButton;
+	private MyCenteredTextButton rollButton;
+	private int endTurnRoll_XPos = 412; 
+	private int endTurnRoll_YPos = 32;
+	private int endTurnRoll_Width = 96;
+	private int endTurnRoll_Height = 32;
 	
-	private MyCenteredTextButton endTurn;
+	private MyCenteredTextBox QPromptBox;
+	private int QPromptBox_XPos = 232;
+	private int QPromptBox_YPos = 160;
+	private int QPromptBox_Width = 176;
+	private int QPromptBox_Height = 32;
+	
+	private MyCenteredTextButton redeemQueueButton;
+	// private groupOfSelectionLists handGroup;
+	private MySelectionList confirmCancelList;
+	
+	
+	// private MyPokerBox pokerBox;
+	private int rulesButton_XPos = 464;
+	private int rulesButton_YPos = 392;
+	private int rulesButton_Width = 160;
+	private int rulesButton_Height = 32;
+	
+	private int exitButton_XPos = 464;
+	private int exitButton_YPos = 432;
+	private int exitButton_Width = 160;
+	private int exitButton_Height = 32;
+
+	private MyCenteredTextButton rulesButton;
+	private MyCenteredTextButton exitButton;
+	
+	private MyCenteredTextBox finishedRedeemBox;
+	private int finishedRedeemBox_XPos = 248;
+	private int finishedRedeemBox_YPos = 96;
+	private int finishedRedeemBox_Width = 144;
+	private int finishedRedeemBox_Height = 32;
+	
+	
+	private boolean hasRolled;
+	private boolean hasRedeemed;
+	private boolean hasFinishedRedeeming;
 	
 	public My37TestState(int main)
 	{}
@@ -30,7 +69,166 @@ public class My37TestState implements GameState
 	@Override
 	public void init(GameContainer gameContainer, StateBasedGame stateGame) throws SlickException 
 	{}
+		
+	// methods for different turn "states"
+	// Update
+ 	public void redeemingUpdate (int clickPositionX, int clickPositionY) 
+	{
+ 		// hasRedeemed && !hasFinishedRedeeming, roll state doesn't matter
+ 		// handGroup.isWithinBound();
+ 		// if(all grounds have something selected)
+ 		// {
+ 		//		confirmCancel.isWithinBound(clickPositionX, clickPositionY);	
+ 		// }
+ 		// actions
+ 		// on confirm special actions based on choices
+ 		// transitions
+ 		// if (confirm)
+ 		// 		hasFinishedRedeeming = true;
+ 		// if (cancel)
+ 		// 		hasRedeemed = false;
+	}
+ 	
+	public void noRedeemUpdate(int clickPositionX, int clickPositionY)
+	{
+		diceHandler.isWithinBound(clickPositionX, clickPositionY);
+		// if(diceHandler selected != 0)
+		// {
+		// 		endTurnButton.isWithinBound(clickPositionX, clickPositionY);
+		// 		if(queueAnalyzerBox isn't none)
+		// 		{
+		// 			redeemButton.isWithinBound(clickPositionX, clickPositionY);
+		// 		}
+		// }
+		largeQueue.isWithinBound(clickPositionX, clickPositionY);
+		
+		// actions
+		// select LargeQueueValues to be redeemed
+		// select DieValue to add to score and to add to queue
+		
+		// transitions
+		// if(endTurnButtonSelected)
+		// 		endTurn
+		// if(redeem)
+		// 		hasRedeemed = true;
+	}
 
+	public void noRollUpdate (int clickPositionX, int clickPositionY)
+	{
+		if(rollButton.isWithinBound(clickPositionX, clickPositionY))
+		{
+			diceHandler.rolled = true;
+			hasRolled = true;
+		}
+		// no actions except transition 
+		
+		// transitions
+		// if(roll)
+		// 		hasRolled = true;
+	}
+	
+	public void neitherUpdate(int clickPositionX, int clickPositionY)
+	{
+		if(rollButton.isWithinBound(clickPositionX, clickPositionY))
+		{
+			diceHandler.rollDice();
+			hasRolled = true;
+		}
+		largeQueue.isWithinBound(clickPositionX, clickPositionY);
+		//redeemButton.isWithinBound(clickPositionX, clickPositionY);
+		
+		// actions
+		// select largeQueue values to redeem
+		
+		// transitions
+		// if(roll)
+		// 		hasRolled = true;
+		// if(redeem)
+		// 		hasRedeemed = true;
+	}
+	
+	public void bothUpdate (int clickPositionX, int clickPositionY) 
+	{
+		// diceHandler.isWithinBound(clickPositionX, clickPositionY);
+		// if(diceHandler selected != 0)
+		// {
+		//  	endTurnButton.isWithinBound(clickPositionX, clickPositionY); 		
+		// }
+		
+		// actions
+		// select die values, end turn
+		
+		// transitions
+		// if(endTurn)
+		// 		endTurn;
+	}
+	
+	// Rendering
+ 	public void redeemingRender (GameContainer gameContainer, StateBasedGame stateGame, Graphics g) 
+	{
+ 		largeQueue.render(gameContainer, stateGame, g);
+ 		QPromptBox.render(gameContainer, stateGame, g);
+ 		// handGroup.render();
+ 		// if(all groups have something selected)
+ 		// {
+ 		// 		confirmCancel.render();
+ 		// }
+	}
+ 	
+	public void noRedeemRender(GameContainer gameContainer, StateBasedGame stateGame, Graphics g)
+	{
+		// if DiceHandler selected != 0
+		// {
+		// 		endTurn.render();
+		//      if(queueAnalyzerBox isn't none)
+		// 		{
+		// 			redeemButton.render();
+		// 		}
+		// }
+		largeQueue.render(gameContainer, stateGame, g);
+		QPromptBox.render(gameContainer, stateGame, g);
+	}
+
+	public void noRollRender (GameContainer gameContainer, StateBasedGame stateGame, Graphics g)
+	{
+		rollButton.render(gameContainer, stateGame, g);
+		finishedRedeemBox.render(gameContainer, stateGame, g);
+	}	
+	
+	public void neitherRender(GameContainer gameContainer, StateBasedGame stateGame, Graphics g)
+	{
+		rollButton.render(gameContainer, stateGame, g);
+		largeQueue.render(gameContainer, stateGame, g);
+		QPromptBox.render(gameContainer, stateGame, g);
+		// if(queueAnalyzerBox isn't none)
+		// {
+		// 		redeemButton.render(gameContainer, stateGame, g);
+		// }
+	}
+	
+	public void bothRender (GameContainer gameContainer, StateBasedGame stateGame, Graphics g) 
+	{
+		// if(diceHandlerSelected != 0)
+		// {
+		// 		endTurn.render(gameContainer, stateGame, g);
+		// }
+		finishedRedeemBox.render(gameContainer, stateGame, g);
+	}
+	
+	// always render method
+	public void alwaysRender (GameContainer gameContainer, StateBasedGame stateGame, Graphics g)
+	{
+		diceHandler.render(gameContainer, stateGame, g);
+		player1.render(gameContainer, stateGame, g);
+		player2.render(gameContainer, stateGame, g);
+		player3.render(gameContainer, stateGame, g);
+		player4.render(gameContainer, stateGame, g);
+		// pokerBox.render(gameContainer, stateGame, g);
+		rulesButton.render(gameContainer, stateGame, g);
+		exitButton.render(gameContainer, stateGame, g);
+		
+	}
+	
 	@Override
 	public void update(GameContainer gameContainer, StateBasedGame stateGame, int delta) throws SlickException 
 	{
@@ -40,8 +238,31 @@ public class My37TestState implements GameState
 			int mouseX = input.getMouseX();
 			int mouseY = input.getMouseY();
 			
+			if(hasRedeemed && !hasFinishedRedeeming)
+			{
+				redeemingUpdate(mouseX, mouseY);
+			}
+			else if (hasRolled && !hasRedeemed)
+			{
+				noRedeemUpdate(mouseX, mouseY);
+			}
+			else if (!hasRolled && hasRedeemed)
+			{
+				noRollUpdate(mouseX, mouseY);
+			}
+			else if (!hasRolled && !hasRedeemed)
+			{
+				neitherUpdate(mouseX, mouseY);
+			}
+			else // hasRolled && hasRedeemed
+			{
+				bothUpdate(mouseX, mouseY);
+			}
+			
+			/*
 			largeQueue.isWithinBound(mouseX, mouseY);
 			diceHandler.isWithinBound(mouseX, mouseY);
+			*/
 			// don't render endTurn button yet
 			
 			// ask the game name box if its been clicked
@@ -56,18 +277,36 @@ public class My37TestState implements GameState
 	@Override
 	public void render(GameContainer gameContainer, StateBasedGame stateGame, Graphics g) throws SlickException 
 	{
-		g.setBackground(Color.white);
+		g.setBackground(Color.white); // move to enter??
+		alwaysRender(gameContainer, stateGame, g);
+		if(hasRedeemed && !hasFinishedRedeeming)
+		{
+			redeemingRender(gameContainer, stateGame, g);
+		}
+		else if (hasRolled && !hasRedeemed)
+		{
+			noRedeemRender(gameContainer, stateGame, g);
+		}
+		else if (!hasRolled && hasRedeemed)
+		{
+			noRollRender(gameContainer, stateGame, g);
+		}
+		else if (!hasRolled && !hasRedeemed)
+		{
+			neitherRender(gameContainer, stateGame, g);
+		}
+		else // hasRolled && hasRedeemed
+		{
+			bothRender(gameContainer, stateGame, g);
+		}
+		
+		/*
 		if (largeQueue != null)
 			largeQueue.render(gameContainer, stateGame, g);
 		if(player1 != null)
 			player1.render(gameContainer, stateGame, g);
 		if(diceHandler != null)
 			diceHandler.render(gameContainer, stateGame, g);
-		/*
-		if (C2c4Box != null)
-			C2c4Box.render(gameContainer, stateGame, g);
-		if (gameStartButton != null)
-			gameStartButton.render(gameContainer, stateGame, g);
 		*/
 	}
 	
@@ -78,10 +317,20 @@ public class My37TestState implements GameState
 		MyDiceGame.initializeFonts();
 		largeQueue = new MyLargeQueue(MyDiceGame.mediumFont);
 		player1 = new MyPlayer("Player 1", 1, MyDiceGame.smallFont);
+		player2 = new MyPlayer("Player 2", 2, MyDiceGame.smallFont);
+		player3 = new MyPlayer("Player 3", 3, MyDiceGame.smallFont);
+		player4 = new MyPlayer("Player 4", 4, MyDiceGame.smallFont);
 		diceHandler = new MyDiceHandler(MyDiceGame.largeFont, MyDiceGame.smallFont);
-		endTurn = new MyCenteredTextButton("End Turn", endTurn_XPos, endTurn_YPos, endTurn_Width, endTurn_Height, MyDiceGame.smallFont);
-		// C2c3Box = new MyCenteredTextBox ("5", C2C3BOX_XPOS, C2C3BOX_YPOS, C2C3BOX_WIDTH, C2C3BOX_HEIGHT, MyDiceGame.smallFont);
-		// C2c4Box = new MyCenteredTextBox ("9", C2C4BOX_XPOS, C2C4BOX_YPOS, C2C4BOX_WIDTH, C2C4BOX_HEIGHT, MyDiceGame.smallFont);
+		endTurnButton = new MyCenteredTextButton("End Turn", endTurnRoll_XPos, endTurnRoll_YPos, endTurnRoll_Width, endTurnRoll_Height, MyDiceGame.smallFont);
+		rollButton = new MyCenteredTextButton("Roll", endTurnRoll_XPos, endTurnRoll_YPos, endTurnRoll_Width, endTurnRoll_Height, MyDiceGame.smallFont);
+		QPromptBox = new MyCenteredTextBox("None", QPromptBox_XPos, QPromptBox_YPos, QPromptBox_Width, QPromptBox_Height, MyDiceGame.smallFont);
+		rulesButton = new MyCenteredTextButton("View Rules", rulesButton_XPos, rulesButton_YPos, rulesButton_Width, rulesButton_Height, MyDiceGame.smallFont);
+		exitButton = new MyCenteredTextButton("Return to Main", exitButton_XPos, exitButton_YPos, exitButton_Width, exitButton_Height, MyDiceGame.smallFont);
+		finishedRedeemBox = new MyCenteredTextBox("Redeemed", finishedRedeemBox_XPos, finishedRedeemBox_YPos, finishedRedeemBox_Width, finishedRedeemBox_Height, MyDiceGame.smallFont); 
+		
+		hasRolled = false;
+		hasRedeemed = false;
+		hasFinishedRedeeming = false;
 	}
 
 	@Override
--- CSE360TeamProject/src/cse360/team6/myCode/MyDiceHandler.java
+++ CSE360TeamProject/src/cse360/team6/myCode/MyDiceHandler.java
@@ -15,14 +15,8 @@ public class MyDiceHandler
 	private int DieButton_Width = 64;
 	private int DieButton_Height = 64;
 	
-	private int RollButton_XPos = 412;
-	private int RollButton_YPos = 32;
-	private int RollButton_Width = 96;
-	private int RollButton_Height = 32;
-	
 	private MyCenteredTextButton Die1Button;
 	private MyCenteredTextButton Die2Button;
-	private MyCenteredTextButton RollButton;
 	
 	boolean rolled;
 	
@@ -40,59 +34,47 @@ public class MyDiceHandler
 		
 		Die1Button = new MyCenteredTextButton("", Die1Button_XPos, DieButton_YPos, DieButton_Width, DieButton_Height, lFont);
 		Die2Button = new MyCenteredTextButton("", Die2Button_XPos, DieButton_YPos, DieButton_Width, DieButton_Height, lFont);
-		RollButton = new MyCenteredTextButton("Roll", RollButton_XPos, RollButton_YPos, RollButton_Width, RollButton_Height, sFont);
 	}
 	
-	public void rollDie()
+	public void rollDice()
 	{
 		die1 = rand.nextInt(9) + 1;
 		die2 = rand.nextInt(9) + 1;
 		rolled = true;
+		Die1Button.SetText("" + die1);
+		Die2Button.SetText("" + die2);
 	}
 
 	public void render(GameContainer gameContainer, StateBasedGame game, Graphics g)
 	{
 		Die1Button.render(gameContainer, game, g);
 		Die2Button.render(gameContainer, game, g);
-		if(!rolled)
-		{
-			RollButton.render(gameContainer, game, g);
-		}
 	}
 	
-	public void isWithinBound(int clickPositionX, int clickPositionY)
+	public boolean isWithinBound(int clickPositionX, int clickPositionY)
 	{
-		if(!rolled)
+		boolean result = false;
+		if(Die1Button.isWithinBound(clickPositionX, clickPositionY))
 		{
-			if(RollButton.isWithinBound(clickPositionX, clickPositionY))
-			{
-				rollDie();
-				Die1Button.SetText(die1 + "");
-				Die2Button.SetText(die2 + "");
-				rolled = true;
-			}
-		}
-		else // rolled
-		{
-			if(Die1Button.isWithinBound(clickPositionX, clickPositionY))
-			{
-				Die1Button.InvertSelection();
-				if(selected > 0)
-				{
-					Die2Button.InvertSelection();
-				}
-				selected = 1;
-			}
- 
-			else if (Die2Button.isWithinBound(clickPositionX, clickPositionY))
+			Die1Button.InvertSelection();
+			if(selected > 0)
 			{
 				Die2Button.InvertSelection();
-				if(selected > 0)
-				{
-					Die1Button.InvertSelection();
-				}
-				selected = 1;				
 			}
+			selected = 1;
+			result = true;
 		}
+
+		else if (Die2Button.isWithinBound(clickPositionX, clickPositionY))
+		{
+			Die2Button.InvertSelection();
+			if(selected > 0)
+			{
+				Die1Button.InvertSelection();
+			}
+			selected = 1;	
+			result = true;
+		}
+		return result;
 	}
 }
