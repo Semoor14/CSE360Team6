7c87c7e11ef38bf6ae31e64dd0b162186d056f02
Kyle Chapman <xeuoruxgmail.com>
2016-04-15 21:03:38 -0700

    DiceGame, CenteredTextBox and Button documentation

--- CSE360TeamProject/src/cse360/team6/CenteredTextBox.java
+++ CSE360TeamProject/src/cse360/team6/CenteredTextBox.java
@@ -1,22 +1,35 @@
 package cse360.team6;
 
-import java.awt.Font;
-
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * A textbox that centers the text inside of it. Basic UI component.
+ * @author Kyle Chapman and Cole Stanton
+ */
 public class CenteredTextBox
 {
+	//The string text and other fields that inform how to render the box on screen.
 	protected String text;
 	protected int xPosition;
 	protected int yPosition;
 	protected int width;
 	protected int height;
+	//the font of the text in the box
 	protected TrueTypeFont textFont;
 	
+	/**
+	 * Constructor for the CenteredTextBox.
+	 * @param text The text to be displayed in the both.
+	 * @param xPos The x positon of the box on the screen.
+	 * @param yPos The y positon of the box on the screen.
+	 * @param width The width of the box on the screen.
+	 * @param height The height of the box on the screen.
+	 * @param font The font to display the text in.
+	 */
 	public CenteredTextBox(String text, int xPos, int yPos, int width, int height, TrueTypeFont font)
 	{
 		this.text = text;
@@ -27,6 +40,12 @@ public class CenteredTextBox
 		this.textFont = font;
 	}
 	
+	/**
+	 * Renders the text box to the screen.
+	 * @param gameContainer The container for the whole game.
+	 * @param game The StateBasedGame handler.
+	 * @param g The graphics object.
+	 */
 	public void render(GameContainer gameContainer, StateBasedGame game, Graphics g)
 	{
 		g.setColor(Color.black);
--- CSE360TeamProject/src/cse360/team6/DiceGame.java
+++ CSE360TeamProject/src/cse360/team6/DiceGame.java
@@ -8,31 +8,38 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * The main class of the DiceBased game 36.
+ * Comntains the main method and methods to initialize the game and its states.
+ * @author Kyle Chapman
+ */
 public class DiceGame extends StateBasedGame
 {
+	//the IDs of the various gamestates
 	public static final int MAIN = 0;
 	public static final int SETUP = 1;
 	public static final int RULES = 2;
 	public static final int STATISTICS = 3;
 	public static final int GAME_37 = 4;
 	
+	//the width, height, and target FPS of the game window
 	public static final int WIDTH   = 640;
     public static final int HEIGHT  = 640;
     public static final int FPS     = 60;
     
+    //global references to the three fonts used in the game
     public static TrueTypeFont smallFont;
     public static TrueTypeFont mediumFont;
     public static TrueTypeFont largeFont;
 	
 	public static void main (String[] args)
 	{
-	
+		//makes a new AppGameContainer, sets its properties, and begins the game
 		try
 		{
 			AppGameContainer gameContainer = new AppGameContainer(new DiceGame("37 Game"));
 			gameContainer.setDisplayMode(WIDTH, HEIGHT, false);
 			gameContainer.setTargetFrameRate(FPS);
-			gameContainer.setShowFPS(true);
 			gameContainer.start();
 		} catch (SlickException e)
 		{
@@ -40,11 +47,19 @@ public class DiceGame extends StateBasedGame
 		}
 	}
 	
+	/**
+	 * Create a new DiceGame object.
+	 * @param name The name of the game.
+	 */
 	public DiceGame(String name)
 	{
 		super(name);
 	}
 
+	/**
+	 * Called by the Slick framework. Initializes the various states of the game.
+	 * @param GameContainer the AppGameContainer window that contains our game.
+	 */
 	@Override
 	public void initStatesList(GameContainer gameContainer) throws SlickException
 	{
@@ -54,5 +69,4 @@ public class DiceGame extends StateBasedGame
         this.addState(new StatisticsGameState(STATISTICS));
         this.addState(new Game37GameState(GAME_37));
 	}
-
 }
